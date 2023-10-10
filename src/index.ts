1a9061c1c1f189b78145960e9baa93041769a389
Clinton Selke <clinuxrulz@gmail.com>
2023-10-10 12:54:33 +1000

    efficent movement of float32 array from wasm to js methods

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { greet } from "voxel-rust-lib";
+import init, * as VoxelLib from "voxel-rust-lib";
 
-greet();
+init().then((module) => {
+    VoxelLib.init();
+    let f32_vec = VoxelLib.new_float32_vec();
+    VoxelLib.float32_vec_write_test_data(f32_vec);
+    let f32_vec_address = VoxelLib.float32_vec_data_address(f32_vec);
+    let f32_vec_len = VoxelLib.float32_vec_len(f32_vec);
+    let f32_vec_js = new Float32Array(module.memory.buffer, f32_vec_address, f32_vec_len);
+    console.log(f32_vec_js);
+});
--- voxel-rust-lib/src/lib.rs
+++ voxel-rust-lib/src/lib.rs
@@ -3,11 +3,42 @@ mod utils;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn init() {
+    utils::set_panic_hook();
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, voxel-rust-lib!");
+pub fn new_float32_vec() -> *mut Vec<f32> {
+    Box::into_raw(Box::new(Vec::new()))
+}
+
+#[wasm_bindgen]
+pub fn free_float32_vec(v: *mut Vec<f32>) {
+    let _ = unsafe { Box::from_raw(v) };
+}
+
+#[wasm_bindgen]
+pub fn float32_vec_data_address(v: *mut Vec<f32>) -> *mut f32 {
+    let v = unsafe { &mut *v };
+    v.as_mut_ptr()
+}
+
+#[wasm_bindgen]
+pub fn float32_vec_len(v: *const Vec<f32>) -> usize {
+    let v = unsafe { &*v };
+    v.len()
+}
+
+#[wasm_bindgen]
+pub fn float32_vec_write_test_data(v: *mut Vec<f32>) {
+    let v = unsafe { &mut *v };
+    let test_data: [f32; 12] = [
+        -1.0, -1.0,  1.0, // v0
+        1.0, -1.0,  1.0,  // v1
+        1.0,  1.0,  1.0,  // v2
+       -1.0,  1.0,  1.0,  // v3
+    ];
+    for x in test_data {
+        v.push(x)
+    }
 }
